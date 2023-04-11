6a2884077d06bf3e9a482c7d2cb5f8fb51fccfa8
brightDeanz <konstantinos.vroustouris@gmail.com>
2023-04-11 12:14:11 +0300

    q5 done

---
+++ q5.js
@@ -0,0 +1,12 @@
+function myFunction(num, num1) {
+    console.log(num1)
+}
+
+let num = 10
+let num1 = 2
+
+myFunction(num)
+
+//expected undefined because we pass num as the first argument and we leave the second arg empty, so it prints out undefined
+
+//got undefined
