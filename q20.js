3a9fe5349889783fb9cb0ebb5e678fe773489666
Konstantinos Vrs <it21712@hua.gr>
2023-04-11 13:10:36 +0300

    q19 done

---
+++ q20.js
@@ -0,0 +1,13 @@
+function myFunction(num1, num2, num3) {
+    return num1 + num3
+  }
+  
+  const num1 = 20
+  const num2 = 200
+  const num3 = 1000
+  
+  const result = myFunction(5, 10, num3, 15)
+
+  //expected 1005
+
+  console.log(result)
