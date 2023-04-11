5988b19da8efabf879a1f99a590309e4787d1add
brightDeanz <konstantinos.vroustouris@gmail.com>
2023-04-11 12:24:08 +0300

    q10 done

---
+++ q10.js
@@ -0,0 +1,13 @@
+function myFunction(num1, num2, num3) {
+    console.log(num3)
+}
+
+const num1 = 10
+const num2 = 1
+const num3 = 20
+
+myFunction(num3, num1, 100)
+
+//expected 100, as the function prints out the last argument
+
+//got 100
