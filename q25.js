05fe6fac9deab35d46adc1a12f2b4d8ae201806f
Konstantinos Vrs <it21712@hua.gr>
2023-04-11 13:16:16 +0300

    q25 done

---
+++ q25.js
@@ -0,0 +1,15 @@
+function myFunction(a) {
+    let b = 20
+    
+    console.log("a:", a)
+    console.log("b:", b)
+    console.log("c:", c)
+  }
+  
+  let a = 1
+  let b = 2
+  let c = 3
+  
+  myFunction(100)
+
+  //expected a:100 b: 20 c:3
