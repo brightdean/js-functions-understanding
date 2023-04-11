e68fbede69807e9cbf520a9403580e1d61248a4a
brightDeanz <konstantinos.vroustouris@gmail.com>
2023-04-11 12:20:02 +0300

    q8 done

---
+++ q8.js
@@ -0,0 +1,15 @@
+function myFunction(num1, num2) {
+    return num1 + num2
+}
+
+const num1 = 10
+const num2 = 1
+const num3 = 4
+
+const result = myFunction(num3, num1)
+
+//expected: 14 because we add num3 and num1 namely 10 + 4
+
+console.log(result)
+
+//got 14
