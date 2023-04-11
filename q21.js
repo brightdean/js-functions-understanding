69bbb9e7d57aa173e809f4f809dc0422153b5ad2
Konstantinos Vrs <it21712@hua.gr>
2023-04-11 13:12:01 +0300

    q21 done

---
+++ q21.js
@@ -0,0 +1,11 @@
+function myFunction(num1, num2) {
+    const result = num1+num2
+    return result
+  }
+  
+  const result = myFunction(10, 20)
+  myFunction(100, 2)
+
+  //expected 30
+
+  console.log(result)
