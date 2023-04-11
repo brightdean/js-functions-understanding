c13fbd3e510448a9be26e0494719ed0d0ea88f79
Konstantinos Vrs <it21712@hua.gr>
2023-04-11 13:04:09 +0300

    q17 done

---
+++ q17.js
@@ -0,0 +1,11 @@
+function myFunction(num1) {
+    return -100
+  
+    return num1 * 10
+  }
+  
+  const result = myFunction(5)
+
+  //expected -100 because the first statement is always executed
+
+  console.log(result)
